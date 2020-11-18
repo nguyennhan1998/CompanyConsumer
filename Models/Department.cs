61edcbf45b5d3f7bc3f226417550bc89be6edd04
nhan nguyen <nhan nguyen@DESKTOP-QRDST9N>
2020-11-18 10:45:18 +0700

    first commit

---
+++ Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompanyConsumer.Models
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CompanyConsumer.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public int salary { get; set; }
+        public int DepartmentID { get; set; }
+        public virtual Department Department { get; set; }
+    }
+}
